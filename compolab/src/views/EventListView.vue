30a314f446e1becac0b6c5d25990602455458110
Jasmxnej <supharat_saelee@cmu.ac.th>
2024-08-07 19:31:53 +0700

    add axios

---
+++ compolab/src/views/EventListView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import type { Event } from '@/types'
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+const events = ref<Event[]>(null)
+
+onMounted (() => {
+  axios
+    .get('[https://my-json-server.typicode.com/Jasmxnej/048/events]')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+const specificEventId = 12345
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <EventDetails :events="events" :eventId="specificEventId" />
+  
+  </div>
+</template>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
