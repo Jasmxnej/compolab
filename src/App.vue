0f46c24cc0cde9665f72fab8f1dd4bb3154a503f
Jasmxnej <supharat_saelee@cmu.ac.th>
2024-08-09 09:31:48 +0700

    add deploy with vercel

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from './stores/message'
-import { storeToRefs } from 'pinia';
+import { storeToRefs } from 'pinia'
 
 const store = useMessageStore();
 const { message } = storeToRefs(store);
@@ -13,6 +13,7 @@ const { message } = storeToRefs(store);
       <div id="flashMessage" class="animate-fade mb-4 py-2 px-4" v-if="message">
         <h4>{{ message }}</h4>
       </div>
+      <h1>Deploy with Vercel</h1>
       <div class="wrapper">
         <nav class="py-6">
           <RouterLink class="font-bold text-gray-700 hover:text-green-500" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">
